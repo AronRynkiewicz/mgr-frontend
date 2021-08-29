5e72e083b1023a7e060bf15c1acf5036c04324d8
Aron <aron.rynkiewicz@gmail.com>
2021-08-29 11:36:52 +0200

    Update Tree.js
    - added display for more taxonomy info

--- src/Tools/Tree.js
+++ src/Tools/Tree.js
@@ -17,19 +17,22 @@ const Tree = (props) => {
     const expandingNodes = nodes.filter((x) => !expanded.includes(x));
     setExpanded(nodes);
     if (expandingNodes[0]) {
-      axios
-        .post("api/browse/", data)
-        .then((resp) =>
-          setChildNodes(
-            JSON.parse(resp.request.response).map((node) => (
-              <Tree
-                id={node}
-                key={Math.floor(Math.random() * 50000000).toString()}
-                name={node}
-              />
-            ))
-          )
-        );
+      axios.post("api/browse/", data).then((resp) =>
+        setChildNodes(
+          JSON.parse(resp.request.response).map((node) => (
+            <Tree
+              id={node.tax_name}
+              key={Math.floor(Math.random() * 50000000).toString()}
+              name={
+                <>
+                  (<b>{node.tax_rank}</b>) {node.tax_name}{" "}
+                  <i>(taxid: {node.tax_id})</i>
+                </>
+              }
+            />
+          ))
+        )
+      );
     }
   };
 
