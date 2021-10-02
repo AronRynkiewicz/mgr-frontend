e1cef69a40d100b1483381e39fdf66b79e2723a0
Aron <aron.rynkiewicz@gmail.com>
2021-10-02 10:10:11 +0200

    Update Download.js
    - removed commented code

--- src/UI/Download.js
+++ src/UI/Download.js
@@ -5,45 +5,6 @@ const Download = (props) => {
     <div>
       <DownloadButton fileType="database" fileName="interactions.json" />
       <DownloadButton fileType="sequences" fileName="sequences.fa" />
-      {/* <button
-        onClick={() => {
-          fetch("api/download/", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ file: "database" }),
-          })
-            .then((response) => {
-              return response.blob();
-            })
-            .then((data) => {
-              saveAs(data, "interactions.json");
-            });
-        }}
-      >
-        Download database (JSON)
-      </button>
-
-      <button
-        onClick={() => {
-          fetch("api/download/", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ file: "sequences" }),
-          })
-            .then((response) => {
-              return response.blob();
-            })
-            .then((data) => {
-              saveAs(data, "sequences.fa");
-            });
-        }}
-      >
-        Download sequences
-      </button> */}
     </div>
   );
 };
